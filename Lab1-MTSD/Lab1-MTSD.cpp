5608985455d5bd32abd7a6f36aff2bb2b828105d
Maks <maxim.kryvenok@gmail.com>
2023-03-07 07:27:26 +0200

    Refactor input modes code into two functions

--- Lab1-MTSD/Lab1-MTSD.cpp
+++ Lab1-MTSD/Lab1-MTSD.cpp
@@ -5,57 +5,21 @@
 #include <exception>
 #include "QuadraticEquation.h"
 
+bool GetFromInput(int*, int*, int*);
+bool GetFromFile(char*, int*, int*, int*);
+
 int main(int argc, char* argv[])
 {
-    if(argc == 2)
-    { 
-        FILE* stream = nullptr;
-        fopen_s(&stream, argv[1], "r");
-        if (stream == NULL) {
-            std::cout << "file " << argv[1] << " does not exist";
-            return 0;
-        }
-        int a = 0, b = 0, c = 0;
-        if(fwscanf_s(stream, L"%d %d %d\n", &a, &b, &c) == 0)
-            std::cout << "invalid file format";
-    }
-
-
     int a = 0, b = 0, c = 0;
+    bool res = false;
+    if (argc == 2)
+        res = GetFromFile(argv[1], &a, &b, &c);
+    else
+        res = GetFromInput(&a, &b, &c);
 
-    std::wcout << L"\x1b[0ma = \x1b[1;32m";
-    while (wscanf_s(L"%d", &a) != 1) {
-        wchar_t inputbuff[256]{};
-        wscanf_s(L"%s", inputbuff, 256);
-        std::wcout << L"Error. Expected a valid real number, got " << inputbuff << " instead" << std::endl;
-        while ((getchar()) != '\n');
+    if (!res)
+        return 0;
 
-        std::wcout << L"\x1b[0ma = \x1b[1;32m";
-        std::wcout << L"\x1b[0m";
-    }
-
-    std::wcout << L"\x1b[0mb = \x1b[1;32m";
-    while (wscanf_s(L"%d", &b) != 1) {
-        wchar_t inputbuff[256]{};
-        wscanf_s(L"%s", inputbuff, 256);
-        std::wcout << L"Error. Expected a valid real number, got " << inputbuff << " instead" << std::endl;
-        while ((getchar()) != '\n');
-
-        std::wcout << L"\x1b[0mb = \x1b[1;32m";
-        std::wcout << L"\x1b[0m";
-    }
-
-    std::wcout << L"\x1b[0mc = \x1b[1;32m";
-    while (wscanf_s(L"%d", &c) != 1) {
-        wchar_t inputbuff[256]{};
-        wscanf_s(L"%s", inputbuff, 256);
-        std::wcout << L"Error. Expected a valid real number, got " << inputbuff << " instead" << std::endl;
-        while ((getchar()) != '\n');
-
-        std::wcout << L"\x1b[0mc = \x1b[1;32m";
-        std::wcout << L"\x1b[0m";
-    }
-    std::wcout << L"\x1b[0m";
 
     wchar_t eqBuff[256]{};
     int rootNumber = 0;
@@ -81,6 +45,58 @@ int main(int argc, char* argv[])
     if (rootNumber >= 2)
         std::wcout << L"x2 = " << x2 << std::endl;
 
+}
+
+bool GetFromInput(int* a, int* b, int* c) {
+    std::wcout << L"\x1b[0ma = \x1b[1;32m";
+    while (wscanf_s(L"%d", a) != 1) {
+        wchar_t inputbuff[256]{};
+        wscanf_s(L"%s", inputbuff, 256);
+        std::wcout << L"Error. Expected a valid real number, got " << inputbuff << " instead" << std::endl;
+        while ((getchar()) != '\n');
+
+        std::wcout << L"\x1b[0ma = \x1b[1;32m";
+        std::wcout << L"\x1b[0m";
+    }
+
+    std::wcout << L"\x1b[0mb = \x1b[1;32m";
+    while (wscanf_s(L"%d", b) != 1) {
+        wchar_t inputbuff[256]{};
+        wscanf_s(L"%s", inputbuff, 256);
+        std::wcout << L"Error. Expected a valid real number, got " << inputbuff << " instead" << std::endl;
+        while ((getchar()) != '\n');
+
+        std::wcout << L"\x1b[0mb = \x1b[1;32m";
+        std::wcout << L"\x1b[0m";
+    }
+
+    std::wcout << L"\x1b[0mc = \x1b[1;32m";
+    while (wscanf_s(L"%d", c) != 1) {
+        wchar_t inputbuff[256]{};
+        wscanf_s(L"%s", inputbuff, 256);
+        std::wcout << L"Error. Expected a valid real number, got " << inputbuff << " instead" << std::endl;
+        while ((getchar()) != '\n');
+
+        std::wcout << L"\x1b[0mc = \x1b[1;32m";
+        std::wcout << L"\x1b[0m";
+    }
+    std::wcout << L"\x1b[0m";
+    return true;
+}
+
+bool GetFromFile(char* filePath, int* a, int* b, int* c)
+{
+    FILE* stream = nullptr;
+    fopen_s(&stream, filePath, "r");
+    if (stream == NULL) {
+        std::cout << "file " << filePath << " does not exist" << std::endl;
+        return false;
+    }
+    if (fwscanf_s(stream, L"%d %d %d\n", a, b, c) != 3) {
+        std::cout << "invalid file format" << std::endl;
+        return false;
+    }
+    return true;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
